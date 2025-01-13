c6a6ad5ae3cc93d3cfaede95d71c157eabe32774
victoria <itsfoxiefo@gmail.com>
2025-01-13 12:07:19 +0100

    changed button style

--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -4,30 +4,30 @@ import s from './styles.module.scss';
 import bin from './../../assets/images/bin.svg'
 
 export default function ListItem({ id, title, completed }) {
-    const dispatch = useDispatch();
-  
-    return (
-      <li className={s.listitem}>
-        <span
-          className={s.listitemTitle}
-          onClick={() => dispatch(toggleTodo(id))}
-        >
-          <input
-            className={s.listitemTitleCheckbox}
-            type="checkbox"
-            checked={completed}
-            readOnly
-          />
-          <span className={completed ? s.listitemCompleted : ''}>
-            {title || 'Untitled task'}
-          </span>
+  const dispatch = useDispatch();
+
+  return (
+    <li className={s.listitem}>
+      <span
+        className={s.listitemTitle}
+        onClick={() => dispatch(toggleTodo(id))}
+      >
+        <input
+          className={s.listitemTitleCheckbox}
+          type="checkbox"
+          checked={completed}
+          readOnly
+        />
+        <span className={completed ? s.listitemCompleted : ''}>
+          {title}
         </span>
-        <button
-          className={s.listitemDelete}
-          onClick={() => dispatch(deleteTodo(id))}
-        >
-          <img src={bin} alt="remove" />
-        </button>
-      </li>
-    );
-  }
+      </span>
+      <button
+        className={s.listitemDelete}
+        onClick={() => dispatch(deleteTodo(id))}
+      >
+        <img src={bin} alt="remove" />
+      </button>
+    </li>
+  );
+}
